19a52fbe0e5f5a0f88a9024706f3f5e1fac9ce66
s2337789 <s2337789@fwn-bborg-edu-74-62.fmnsedu.rug.nl>
2013-05-24 09:49:50 +0200

    packages

---
+++ src/HexagonWars/Entities/Infantry.java
@@ -0,0 +1,12 @@
+package HexagonWars.Entities;
+
+import HexagonWars.Entities.Unit;
+
+/**
+ *
+ * @author Patrick Beuks (s2288842), Floris Huizinga (s2397617) and
+ * @author Timo Smit (s2337789)
+ */
+public abstract class Infantry extends Unit {
+
+}
