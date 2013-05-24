ef4317e0c60603643d773f41ebf19c2f592db37f
s2337789 <s2337789@fwn-bborg-edu-74-62.fmnsedu.rug.nl>
2013-05-24 09:36:41 +0200

    concept classes

--- src/HexagonWars/Entity.java
+++ src/HexagonWars/Entity.java
@@ -5,6 +5,6 @@ package HexagonWars;
  * @author Patrick Beuks (s2288842), Floris Huizinga (s2397617) and
  * @author Timo Smit (s2337789)
  */
-public class Entities {
+public abstract class Entity {
     //@TODO test
 }
---
+++ src/HexagonWars/Unit.java
@@ -0,0 +1,10 @@
+package HexagonWars;
+
+/**
+ *
+ * @author Patrick Beuks (s2288842), Floris Huizinga (s2397617) and
+ * @author Timo Smit (s2337789)
+ */
+public abstract class Unit extends Entity {
+    
+}
