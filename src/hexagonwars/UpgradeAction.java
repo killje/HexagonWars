b4163025924df975a9000753fc3c41e965342606
Patrick <Patrick@zolder>
2013-06-20 15:04:52 +0200

    UpgradeAction added

---
+++ src/hexagonwars/UpgradeAction.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hexagonwars;
+
+/**
+ *
+ * @author Patrick Beuks (s2288842), Floris Huizinga (s2397617) and
+ * @author Timo Smit (s2337789)
+ */
+public class UpgradeAction extends UIAction{
+
+    public UpgradeAction(){
+        // TODO code application logic here
+    }
+}
