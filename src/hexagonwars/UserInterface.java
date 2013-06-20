151d424a946c42fbbcdd4ab73ac62df45bd9a901
Timo Smit <t.smitje@hotmail.com>
2013-06-20 12:07:45 +0200

    CamelCase

--- src/hexagonwars/UserInterface.java
+++ src/hexagonwars/UserInterface.java
@@ -1,5 +1,11 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package hexagonwars;
 
+import java.awt.Image;
+import java.awt.Point;
 import java.util.ArrayList;
 
 /**
@@ -12,26 +18,41 @@ public class UserInterface {
     public static final int ICON_WIDTH = 50;
     public static final int ICON_HEIGHT = 50;
     private String name;
-    private ArrayList<ImageWithAction> actionList = new ArrayList<>();
-    private int health;
+    private ArrayList<ImageWithLocation> icons = new ArrayList<>();
 
     public UserInterface(String name) {
         this.name = name;
     }
 
-    public ArrayList<ImageWithAction> getActions() {
-        return actionList;
+    public void addIcon(String iconName,Point p) {
+        icons.add(new ImageWithLocation(HWImage.getImage(1, 1, ICON_WIDTH, ICON_HEIGHT, iconName),p));
+    }
+
+    public ArrayList<ImageWithLocation> getIcons() {
+        return icons;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setHealth(int health) {
-        this.health = health;
-    }
+    public class ImageWithLocation {
 
-    public void addAction(String actionName, UIAction action) {
-        actionList.add(new ImageWithAction(HWImage.getImage(1, 1, ICON_WIDTH, ICON_HEIGHT, actionName), action));
+        Point point;
+        Image image;
+
+        private ImageWithLocation(Image image, Point p) {
+            point = p;
+            this.image = image;
+        }
+        
+        public Image getImage(){
+            return image;
+        }
+        
+        public Point getLocation(){
+            return point;
+        }
+            
     }
 }
