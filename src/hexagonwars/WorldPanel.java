efa8387f6b4ff2264c08551bf479e0eb51c65954
Patrick <Patrick@zolder>
2013-06-24 01:24:09 +0200

    click and place

--- src/hexagonwars/WorldPanel.java
+++ src/hexagonwars/WorldPanel.java
@@ -19,6 +19,7 @@ public class WorldPanel extends MapPanel {
     private GameUI gameUI = new GameUI(this);
     private final Tile[] posibleTiles;
     private ArrayList<Tile> tiles;
+    private boolean drawMoves = false;
 
     public WorldPanel() {
         File file = new File(Paths.get("").toAbsolutePath().toString() + File.separator + "src" + File.separator + "hexagonwars" + File.separator + "maps" + File.separator + "firstmap.hwm");//debug
@@ -32,11 +33,16 @@ public class WorldPanel extends MapPanel {
         posibleTiles[3] = Tile.getTileFromType(WorldTiles.GOLD);
         posibleTiles[4] = Tile.getTileFromType(WorldTiles.SHALLOWS);
         posibleTiles[5] = Tile.getTileFromType(WorldTiles.FOREST);
-        tiles = worldMap.getMoves(posibleTiles, new Point(20, 20), 3);
+
     }
 
     @Override
     protected void tileClick(WorldModel world, Point TileCoordinate) {
+        selectedTile = world.getTile(TileCoordinate.x, TileCoordinate.y);
+        drawMoves = true;
+        if (drawMoves) {
+            tiles = worldMap.getMoves(posibleTiles, worldMap.getTilePosition(selectedTile), 3);
+        }
     }
 
     @Override
@@ -47,11 +53,12 @@ public class WorldPanel extends MapPanel {
         g.drawImage(HWImage.getImageWithDefaultTransparensy("exitButton"), uiRect.x + 50, uiRect.y, null);
         g.drawImage(HWImage.getImageWithDefaultTransparensy("saveButton"), uiRect.x + 100, uiRect.y, null);
         g.drawImage(HWImage.getImageWithDefaultTransparensy("loadButton"), uiRect.x + 150, uiRect.y, null);
-        for (Tile tile : tiles) {
-            System.out.println(worldMap.getTilePosition(tile));
-            drawHex(g, worldMap.getTilePosition(tile).x*(int) (HexagonWars.WORLD_TILE_WIDTH * worldMap.getZoomLevel()) + worldMap.getTilePosition(tile).y % 2 * (int) (HexagonWars.WORLD_TILE_WIDTH / 2 * worldMap.getZoomLevel()),worldMap.getTilePosition(tile).y * (int) (HexagonWars.WORLD_TILE_HEIGHT_MIN * worldMap.getZoomLevel()), new Color(0, 0, 255, 80), worldMap);
+        if (drawMoves) {
+            for (Tile tile : tiles) {
+                System.out.println(worldMap.getTilePosition(tile));
+                drawHex(g, worldMap.getTilePosition(tile).x * (int) (HexagonWars.WORLD_TILE_WIDTH * worldMap.getZoomLevel()) + worldMap.getTilePosition(tile).y % 2 * (int) (HexagonWars.WORLD_TILE_WIDTH / 2 * worldMap.getZoomLevel())+worldMap.getXLocation(), worldMap.getTilePosition(tile).y * (int) (HexagonWars.WORLD_TILE_HEIGHT_MIN * worldMap.getZoomLevel())+worldMap.getYLocation(), new Color(0, 0, 255, 80), worldMap);
+            }
         }
-
     }
 
     public WorldModel getWorldModel() {
