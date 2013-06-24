8101e9cc04b8b90962a6b1e1409e96dfca831080
Timo Smit <t.smitje@hotmail.com>
2013-06-24 07:58:16 +0200

    Javadoc added to GameHandler

--- src/hexagonwars/GameHandler.java
+++ src/hexagonwars/GameHandler.java
@@ -16,22 +16,42 @@ public class GameHandler implements Serializable {
     private ArrayList<Player> players = new ArrayList<>();
     private ArrayList<Building> buildingConstructions = new ArrayList<>();
 
+    /**
+     * Initialization of a Game instance.
+     */
     public GameHandler() {
         turn = 0; /* player one */
     }
 
+    /**
+     * Adds a player to the game.
+     * @param player the Player instance to be added
+     */
     public void addPlayer(Player player) {
         players.add(player);
     }
 
+    /**
+     * Returns the player for the current turn.
+     * @return the current player on turn
+     */
     public Player getCurrentPlayer() {
         return players.get(turn);
     }
 
+    /**
+     * Returns the name for a specific player.
+     * @param player the player instance
+     * @return the name
+     */
     public String getPlayerName(Player player) {
         return player.getName();
     }
 
+    /**
+     * Raises the turn counter by one (or goes back to zero), handles the building
+     * processes and updates all entities - allowing them to do finish their tasks.
+     */
     public void nextTurn() {
         turn++;
         if (turn >= players.size()) {
@@ -54,11 +74,23 @@ public class GameHandler implements Serializable {
         }
     }
 
+    /**
+     * Moves a unit from one tile to another.
+     * @param entity the entity to be moved.
+     * @param currentTile the current tile.
+     * @param newTile the new tile.
+     */
     public void moveUnit(Entity entity, Tile currentTile, Tile newTile) {
         currentTile.removeAllEntities();
         newTile.addEntity(entity);
     }
 
+    /**
+     * Gets a newly spawned entity out of a building.
+     * @param entity the entity to be moved.
+     * @param currentTile the current tile.
+     * @param newTile the new tile.
+     */
     public void moveFromBuilding(Entity entity, Tile currentTile, Tile newTile) {
         Producer producer = (Producer) entity;
         Unit unit = producer.getLastUnit();
@@ -67,6 +99,10 @@ public class GameHandler implements Serializable {
         producer.removeLastUnit();
     }
 
+    /**
+     * Starts the building process of a newly created building.
+     * @param building the building to be built.
+     */
     public void build(Building building) {
         buildingConstructions.add(building);
         getCurrentPlayer().addPlayerEntity(building);
@@ -74,6 +110,12 @@ public class GameHandler implements Serializable {
         building.startBuild();
     }
 
+    /**
+     * A possible implementation of an attack method.
+     * @param tileAttacker the tile where the attacker stood.
+     * @param tileDefender the tile where the defender stands.
+     * @throws Exception if the entity types did not meet any requirements.
+     */
     public void attack(Tile tileAttacker, Tile tileDefender) throws Exception {
         if (!(tileAttacker.getEntity() instanceof Unit)) {
             throw new Exception("The provided attacking entity was not of type Unit");
